8cdef516d7aab8db4afc2cd9fca4435d9aa71300
ahmed1nasrat <ahmednasrat16@gmail.com>
2026-05-20 00:15:44 +0300

    JWT authentication service and configuration

---
+++ job_test/Application/Interfaces/IJwtService.cs
@@ -0,0 +1,8 @@
+﻿namespace job_test.Application.Interfaces
+{
+    public interface IJwtService
+    {
+        string GenerateToken( int userId,string userName,string email);
+    
+    }
+}
---
+++ job_test/Infrastructure/Authentication/JwtService.cs
@@ -0,0 +1,36 @@
+﻿using job_test.Application.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace job_test.Infrastructure.Authentication
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken( int userId, string userName, string email)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Name, userName),
+                new Claim(ClaimTypes.Email, email)
+            };
+
+            var key = new SymmetricSecurityKey( Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+
+            var creds = new SigningCredentials( key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken( issuer: _configuration["Jwt:Issuer"],  audience: _configuration["Jwt:Audience"],  claims: claims,expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:DurationInMinutes"])),signingCredentials: creds );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
